6c61cc498b4911c93d0390df5b6a19c1589aea9a
Matt Titchener <matt.titchener@gmail.com>
2016-08-02 22:29:01 -0600

    Modify project folder structure
    Migrate project folder structure to build front-end assets into a 'dist'
    directory. On the one hand this simplifies the gulp build process by
    centralising all build output---however, it does mean that Bracket's live
    preview is rendered mostly useless.

--- development/front-end/gulpfile.js
+++ development/front-end/gulpfile.js
@@ -4,17 +4,17 @@ var gulp = require("gulp"),
     typogr = require("gulp-typogr"),
     compass = require("gulp-compass"),
     svgmin = require("gulp-svgmin"),
-    sassSourcefiles = "./scss/**/*.scss",
-    htmlFiles = "./**/*.htm",
-    svgFiles = "./svg/**/*.svg";
+    sassSourcefiles = "src/scss/**/*.scss",
+    htmlFiles = "src/**/*.htm",
+    svgFiles = "src/svg/**/*.svg";
 
 gulp.task("compass", function () {
     gulp.src(sassSourcefiles)
         .pipe(compass({
             // The `css` and `sass` directories must be specified both here and in the config.rb file.
             config_file: "./config.rb",
-            css: "css",
-            sass: "scss"
+            css: "./dist/css",
+            sass: "./src/scss"
         }))
         .on('error', function (error) {
             // Do nothing; prevent the gulp task from exiting.
@@ -26,13 +26,13 @@ gulp.task("typeset", function () {
         .pipe(typogr({
             only: ["smartypants"]
         }))
-        .pipe(gulp.dest("./"));
+        .pipe(gulp.dest("./dist"));
 });
 
 gulp.task("optimize-svg", function () {
     return gulp.src(svgFiles)
         .pipe(svgmin())
-        .pipe(gulp.dest("./images/"));
+        .pipe(gulp.dest("./dist/images"));
 });
 
 gulp.task("compass:watch", function () {
