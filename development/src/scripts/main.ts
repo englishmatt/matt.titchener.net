a9428667dcf37eefb5cf321af0dc02d3aab16761
Matt Titchener <matt@titchener.net>
2019-01-24 23:44:02 -0700

    Collapse page header on scroll

--- development/src/scripts/main.ts
+++ development/src/scripts/main.ts
@@ -1,5 +1,8 @@
 (function () {
 
-  // Nothing here
+  // Toggle class when documentElement has been scrolled (or not)
+  window.addEventListener("scroll", () => {
+    document.documentElement.classList.toggle("page-scrolled", window.pageYOffset > 0 ? true : false);
+  });
 
 }());
