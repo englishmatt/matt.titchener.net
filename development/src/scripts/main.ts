bdabb7769639ab3d5a826b92361cd4077253d420
Matt Titchener <matt@titchener.net>
2019-01-25 23:24:24 -0700

    Sync page header transition with scroll progress

--- development/src/scripts/main.ts
+++ development/src/scripts/main.ts
@@ -1,7 +1,21 @@
 (function () {
 
-  // Toggle class when documentElement has been scrolled (or not)
+  let header: HTMLElement = document.querySelector(".page-header"),
+      initialHeaderHeight = header.getBoundingClientRect().height;
+
   window.addEventListener("scroll", () => {
+
+    let headerHeight = window.pageYOffset > initialHeaderHeight ? 0 : initialHeaderHeight - window.pageYOffset,
+        headerFractionalSize = headerHeight / initialHeaderHeight,
+        headerFractionalSizeLimit = 0.5;  // Arbitrary value, chosen for good aesthetic
+
+    headerFractionalSize = headerFractionalSize < headerFractionalSizeLimit ? headerFractionalSizeLimit : headerFractionalSize;
+
+    // Update header layout
+    header.style.height = headerHeight + "px";
+    header.style.fontSize = headerFractionalSize + "rem";
+
+    // Add class to indicate page is in scrolled state
     document.documentElement.classList.toggle("page-scrolled", window.pageYOffset > 0 ? true : false);
   });
 
