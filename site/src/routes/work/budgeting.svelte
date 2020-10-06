90a479a8a634e20060c1076279bc32c736920c16
Matt Titchener <matt@titchener.net>
2020-10-05 22:55:00 -0700

    Tweak Budgeting details copy

--- site/src/routes/work/budgeting.svelte
+++ site/src/routes/work/budgeting.svelte
@@ -72,26 +72,31 @@
         <h2>Opportunity</h2>
         <p>
             I set myself the challenge to design a personal finance and budgeting tool for use on the web and desktop.
-            Given my work experience in the finance industry I had an 'ok' feel for what a product like this might look like:
-            A <strong>simple-as-possible-but-no-simpler</strong> solution that would help individuals and families alike identify, track, and stick to a budget.
+            Given my work experience in the finance industry I had an ‘ok’ feel for what a product like this might look like:
+            A <strong>simple-as-possible-but-no-simpler</strong> solution that would help individuals and families alike
+            identify, track, and stick to a financial budget.
         </p>
         <h2>Process</h2>
         <p>
-            I started by considering how best to build a budget from scratch. This involved
-            forming a clear <strong>mental model</strong> for the user: a budget was
-            determined to be <em>a set of recurring transactions, both income and expense, over a fixed period of time</em>.
-            All other features would successfully hang off this central concept.
+            I started by considering how best to walk a new user through building a budget from scratch. This involved
+            forming a clear <strong>mental model</strong> for the user: a budget was determined to be <em>a set of
+            recurring transactions, both income and expense, over a fixed period of time</em>. All other features would
+            successfully hang off this central concept.
         </p>
         <h3>Visualizations</h3>
         <Figure>
             <Image src="../../budgeting/budgeting-sketch.jpg" alt="" align="left" />
             <p slot="caption">
-                As I was working alone I did not generate much of the research and documentation I normally would. I worked
-                out some quick sketches to think through how best to <strong>visualize</strong> account balances and transactions:
+                As I was working alone I did not generate much of the research and documentation I normally would. I
+                worked out some quick sketches to think through how best to <strong>visualize</strong> account balances
+                and transactions:
             </p>
-        </Figure>        
+        </Figure>
         <h3>Grey Boxing</h3>
-        <p>I skipped much of the lower-fidelity work out as I was working solo and already a good <strong>product vision</strong>. I started <strong>gray boxing</strong> in <strong>Adobe&nbsp;XD</strong>:</p>
+        <p>
+            I already had a solid <strong>product vision</strong>, so felt to skip much of the lower-fidelity work; I
+            started <strong>gray boxing</strong> in <strong>Adobe&nbsp;XD</strong>:
+        </p>
         <Carousel height="30rem">
             <Image alt="" src="../../budgeting/wireframes/overview.png" width="1460" height="840" />
             <Image alt="" src="../../budgeting/wireframes/bank-accounts.png" width="1460" height="840" />
@@ -107,11 +112,11 @@
         <aside>
             <h3>A Word on Grey Boxing</h3>
             <p>
-                I have found the use of grey boxing&mdash;mid-fidelity layouts, sans colours,
-                images, and textures&mdash;more informative and useful than practically any other technique I know. It is useful to
-                quickly get a handle on a user workflow. When I'm short on time or need a more concrete answer as to whether
-                something “works” I turn to grey boxing. <abbr title="Your Mileage May Vary">YMMV</abbr>, but highly
-                recommend giving it a shot for your next project.
+                I have found the use of grey boxing&mdash;<strong>mid-fidelity</strong> layouts, sans colours, images,
+                and textures&mdash;more informative and useful than practically any other technique I know. It is useful
+                to quickly get a handle on a user workflow. When I'm short on time or need a more concrete answer as to
+                whether something “works” I turn to grey boxing. <abbr title="Your Mileage May Vary">YMMV</abbr> of
+                course, but I highly recommend giving it a shot if you want to get started quickly.
             </p>
         </aside>
         <h2>Pattern Library</h2>
@@ -120,9 +125,10 @@
     <section>
         <h2>Outcome</h2>
         <p>
-            Budgeting was a challenge to myself on how best to create clarity in a complex domain: providing concise <strong>mental models</strong> for
-            budgeting; <strong>condensing information</strong> in to clear-to-understand visuals, including <strong>sparklines</strong>,
-            <strong>interactive charts</strong>, and the <strong>use of colour</strong>.
+            Budgeting was a challenge to myself on how best to create clarity in a complex domain: generating concise,
+            internally consistent <strong>mental models</strong> for budgeting; <strong>condensing information</strong>
+            in clear-to-understand visuals, including <strong>sparklines</strong>, <strong>interactive charts</strong>,
+            and the meaningful <strong>use of colour</strong>.
         </p>
     </section>
 </CaseStudy>
