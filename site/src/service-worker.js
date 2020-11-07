d294d47d2d6170d6e7af116404f6568cd6fde4c5
Matt Titchener <matt.titchener@gmail.com>
2020-11-07 14:54:56 -0800

    Format housekeeping

--- site/src/service-worker.js
+++ site/src/service-worker.js
@@ -17,63 +17,63 @@ const cached = new Set(to_cache);
 console.log(shell, files, routes);
 
 self.addEventListener('install', event => {
-	event.waitUntil(
-		caches
-			.open(ASSETS)
-			.then(cache => cache.addAll(to_cache))
-			.then(() => {
-				self.skipWaiting();
-			})
-	);
+    event.waitUntil(
+        caches
+            .open(ASSETS)
+            .then(cache => cache.addAll(to_cache))
+            .then(() => {
+                self.skipWaiting();
+            })
+    );
 });
 
 self.addEventListener('activate', event => {
-	event.waitUntil(
-		caches.keys().then(async keys => {
-			// Delete old caches
-			for (const key of keys) {
-				if (key !== ASSETS) {
-					await caches.delete(key);
-				}
-			}
+    event.waitUntil(
+        caches.keys().then(async keys => {
+            // Delete old caches
+            for (const key of keys) {
+                if (key !== ASSETS) {
+                    await caches.delete(key);
+                }
+            }
 
-			self.clients.claim();
-		})
-	);
+            self.clients.claim();
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
-	if (event.request.method !== 'GET' || event.request.headers.has('range')) return;
+    if (event.request.method !== 'GET' || event.request.headers.has('range')) return;
 
-	const url = new URL(event.request.url);
+    const url = new URL(event.request.url);
 
-	// Don't try to handle e.g. data: URIs
-	if (!url.protocol.startsWith('http')) return;
+    // Don't try to handle e.g. data: URIs
+    if (!url.protocol.startsWith('http')) return;
 
-	// Ignore dev server requests
-	if (url.hostname === self.location.hostname && url.port !== self.location.port) return;
+    // Ignore dev server requests
+    if (url.hostname === self.location.hostname && url.port !== self.location.port) return;
 
-	// Always serve static files and bundler-generated assets from cache
-	if (url.host === self.location.host && cached.has(url.pathname)) {
-		event.respondWith(caches.match(event.request));
-		return;
-	}
+    // Always serve static files and bundler-generated assets from cache
+    if (url.host === self.location.host && cached.has(url.pathname)) {
+        event.respondWith(caches.match(event.request));
+        return;
+    }
 
-	if (event.request.cache === 'only-if-cached') return;
+    if (event.request.cache === 'only-if-cached') return;
 
-	// For everything else, try the cache first, falling back to network
-	event.respondWith(
-		caches
-			.open(`offline${timestamp}`)
-			.then(async cache => {
-				let response = await cache.match(event.request);
-				if (response) {
-					return response;
-				}
+    // For everything else, try the cache first, falling back to network
+    event.respondWith(
+        caches
+            .open(`offline${timestamp}`)
+            .then(async cache => {
+                let response = await cache.match(event.request);
+                if (response) {
+                    return response;
+                }
 
-				response = await fetch(event.request);
-				cache.put(event.request, response.clone());
-				return response;
-			})
-	);
+                response = await fetch(event.request);
+                cache.put(event.request, response.clone());
+                return response;
+            })
+    );
 });
